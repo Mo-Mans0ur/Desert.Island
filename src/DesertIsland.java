a510f2a50c9ed075ff1ddef033089aa5aae85b0c
Mo-Mans0ur <Moha1998@outlook.dk>
2021-09-16 14:15:14 +0200

    getting started

--- src/DesertIsland.java
+++ src/DesertIsland.java
@@ -1,5 +1,10 @@
 public class DesertIsland {
     public static void main(String[] args) {
+        System.out.println("welcome, user if you landed on a desert island ");
+        System.out.println("what would you take with you? ");
+        System.out.println("here's your options");
+
+        String[] options = {"knife", "survival book", "fire starting kit", "cellphone", "gps", "boat", "clothe", "guitar", "pencil and paper", "hammock"};
 
     }
 }
