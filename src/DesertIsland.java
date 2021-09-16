e02cf29e4fa6a03daab3896fcb7fce3001b739b1
Mo-Mans0ur <Moha1998@outlook.dk>
2021-09-16 14:25:03 +0200

    getting started

--- src/DesertIsland.java
+++ src/DesertIsland.java
@@ -2,9 +2,15 @@ public class DesertIsland {
     public static void main(String[] args) {
         System.out.println("welcome, user if you landed on a desert island ");
         System.out.println("what would you take with you? ");
-        System.out.println("here's your options");
+        System.out.println("here's your options: ");
 
-        String[] options = {"knife", "survival book", "fire starting kit", "cellphone", "gps", "boat", "clothe", "guitar", "pencil and paper", "hammock"};
+        String[] options = {"1. knife", "2. survival book", "3. fire starting kit", "4. cellphone", "5. gps", "6. boat", "7. clothe", "8. guitar", "9. pencil and paper", "10. hammock"};
+        System.out.println();
+        System.out.println();
+
+        for (String survivaloptions : options) {
+            System.out.println(survivaloptions);
+        }
 
     }
 }
